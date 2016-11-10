9be2fad608802e1195055ce7dd619690a110bdb8
Lalitwadee Yodnin <57160482@go.buu.ac.th>
2016-11-10 18:47:41 +0700

    เพิ่ม checkbox

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ angular.module ('Contactapp',[])
   .service('contactService',function(){
     var self = this //เก็บ reference this ไว้ก่อน
 
-    self.contacts = [ //ใช้ var ไม่ได้ เพราะ contacts จะถูกสร้างใหม่เสมอ
-      { fullname: 'Lalitwadee Yodnin',nickname: 'Bel',phone: '0807702115'},
-      { fullname: 'Juntana Jumras',nickname: 'Num',phone: '092xxxxxxx'},
-      { fullname: 'Warunee Phosri',nickname: 'Aor',phone: '0981234567'},
-      { fullname: 'Natworapicha Sakamon',nickname: 'Tan',phone: '0671234567'}
+    self.contacts = [
+      {message:"hey yo!"}
     ]
 
     self.list = function(){
@@ -26,10 +23,8 @@ angular.module ('Contactapp',[])
 
   .controller('AddContactController', function ($scope, contactService){
     $scope.add = function(){
-      var contact = {
-        fullname: $scope.fullname,
-        nickname: $scope.nickname,
-        phone: $scope.phone
+    var contact = {
+        message: $scope.message
       }
 
       contactService.add(contact)
@@ -37,8 +32,9 @@ angular.module ('Contactapp',[])
     }
 
     function resetForm() {
-      $scope.fullname = ''
-      $scope.nickname = ''
-      $scope.phone = ''
+      $scope.message = ''
     }
+
+
+
   })
